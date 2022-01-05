8ac4b121690afe77ed6b700adbd3b2a9d048c37a
Hong Sung <hsung@uco.edu>
2022-01-05 10:12:40 -0600

    update

--- public/model/constants.js
+++ public/model/constants.js
@@ -1,13 +1,13 @@
 export const COLLECTION = 'RPI_DATA';
-export const DOCNAME_LEDS = 'DOC_LEDS';
+export const DOCNAME_LEDS = 'DOC_LEDS',
 export const DOCNAME_BUTTONS = 'DOC_BUTTONS'
 
 export const docButtons = {
-  button1_on: false,
-  button2_on: false,
+  button1: false,
+  button2: false,
 }
 
 export const docLEDs = {
-  led1_on: false,
-  led2_on: false,
+  led1: false,
+  led2: false,
 }
