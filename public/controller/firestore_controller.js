fc15f2c6f120b87a383e7478ec83dcf9589b6898
Hong Sung <hsung@uco.edu>
2022-01-05 20:22:57 -0600

    led control

--- public/controller/firestore_controller.js
+++ public/controller/firestore_controller.js
@@ -1,5 +1,5 @@
 import {
-	getFirestore, onSnapshot, doc, setDoc
+	getFirestore, onSnapshot, doc, setDoc, updateDoc
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"
 
 import * as Constants from '../model/constants.js'
@@ -9,6 +9,11 @@ const db = getFirestore();
 export async function initFirestoreDocs() {
 	await setDoc(doc(db, Constants.COLLECTION, Constants.DOCNAME_BUTTONS), Constants.docButtons);
 	await setDoc(doc(db, Constants.COLLECTION, Constants.DOCNAME_LEDS), Constants.docLEDs);
+}
+
+export async function updateDocForLED(update) {
+	const docRef = doc(db, Constants.COLLECTION, Constants.DOCNAME_LEDS);
+	await updateDoc(docRef, update);
 }
 
 export function attachRealtimeListener(collection, document, callback) {
--- public/viewpage/home_page.js
+++ public/viewpage/home_page.js
@@ -1,7 +1,7 @@
 import * as Auth from '../controller/auth.js'
 import * as Elements from './elements.js'
 import * as Constants from '../model/constants.js'
-import { attachRealtimeListener, initFirestoreDocs } from '../controller/firestore_controller.js';
+import {updateDocForLED, attachRealtimeListener, initFirestoreDocs } from '../controller/firestore_controller.js';
 
 const noMonitor = 'No Monitor';
 
@@ -38,6 +38,7 @@ export function home_page() {
 
   let html = '<h3 class="d-flex justify-content-center m-3">Control Panel<h3>';
   html += `
+  <div style="background-color:rgb(245,245,220);">
     <div class="d-flex justify-content-start">
       <h5>Start/Stop Monitor Button Status:</h5>
       <button id="button-monitor-button-status" type="input" class="btn btn-outline-primary ms-3">Start</button>
@@ -51,6 +52,14 @@ export function home_page() {
       <button disabled id="status-button2" type="input" class="btn btn-primary mx-3">
       ${noMonitor}</button>
     </div>
+  </div>
+
+  <div style="background-color:rgb(240,248,255); margin-top: 50px;">
+    <div class="d-flex justify-content-start">
+      <h5>Control LED1</h5>
+      <button id="button-led-control" type="input" class="btn btn-outline-primary ms-3">Turn ON</button>
+    </div>
+  </div>
   `;
 
   Elements.root.innerHTML = html;
@@ -70,6 +79,18 @@ export function home_page() {
       status1.innerHTML = noMonitor;
       status2.innerHTML = noMonitor;
       if (unsubButtonsDoc) unsubButtonsDoc();
+    }
+  });
+
+  const ledButton = document.getElementById('button-led-control');
+  ledButton.addEventListener('click', e => {
+    const label = e.target.innerHTML;
+    if (label == 'Turn ON') {
+      e.target.innerHTML = 'Turn OFF';
+      updateDocForLED({led1: true});
+    } else {
+      e.target.innerHTML = 'Turn ON';
+      updateDocForLED({led1: false});
     }
   });
 
